136029a63c7578351077d7e5ea210e354c18f37f
KuroCaelum <kuro_caelum@outlook.com>
2018-11-20 19:45:17 -0300

    Added AlunoTest

---
+++ src/test/java/br/com/escola/model/domain/AlunoTest.java
@@ -0,0 +1,26 @@
+package br.com.escola.model.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AlunoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetTelefone() {
+//		assertEquals(true, true);
+		fail("Not implemented yet");
+	}
+
+}
